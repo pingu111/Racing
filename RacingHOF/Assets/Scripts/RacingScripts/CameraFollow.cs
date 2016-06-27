1a5108199db296b555471164db358251f1ef9a46
pingu111 <theo.carron@telecom-sudparis.eu>
2016-06-27 11:35:21 +0200

    first Commit

---
+++ RacingHOF/Assets/Scripts/RacingScripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Script that made the Camera following the GameObject 
+ */
+public class CameraFollow : MonoBehaviour {
+
+    // The gameObject that we want to follow 
+    [SerializeField]
+    GameObject objectToFollow;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // We follow the gameObject 
+
+        Vector3 velocity = Vector3.zero;
+        Vector3 forward = objectToFollow.transform.forward * 10.0f;
+        Vector3 needPos = objectToFollow.transform.position - forward;
+        transform.position = Vector3.SmoothDamp(transform.position, needPos,
+                                                ref velocity, 0.05f);
+        transform.LookAt(objectToFollow.transform);
+        transform.rotation = objectToFollow.transform.rotation;
+    }
+}
