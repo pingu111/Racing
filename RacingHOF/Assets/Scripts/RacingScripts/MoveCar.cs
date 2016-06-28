1d1da77f71d2edccace599dc1a223716b58d7a2b
pingu111 <theo.carron@telecom-sudparis.eu>
2016-06-28 13:50:55 +0200

    presque detection du point le plu proche

--- RacingHOF/Assets/Scripts/RacingScripts/MoveCar.cs
+++ RacingHOF/Assets/Scripts/RacingScripts/MoveCar.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class MoveCar : MonoBehaviour
 {
     // the information about each individual axle
+    [SerializeField]
     public List<GameObject> listOfWheels;
 
     // maximum torque the motor can apply to wheel
@@ -15,6 +16,10 @@ public class MoveCar : MonoBehaviour
 
     // the speed of this car
     public float carSpeed;
+
+    // The best line
+    [SerializeField]
+    public GameObject bestLine;
 
     // Called at every physics steps
     public void FixedUpdate()
@@ -35,6 +40,36 @@ public class MoveCar : MonoBehaviour
         }
         else
             carSpeed = 0;
+
+        Vector3 pos = this.transform.position;
+        pos.z = transform.position.z - Camera.main.transform.position.z;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+
+        Vector3 placement = findClosest(bestLine.GetComponent<PolygonCollider2D>(), pos);
+        placement.z = transform.position.z;
+        Debug.Log( placement);
+    }
+
+    private Vector2 findClosest(PolygonCollider2D collider, Vector2 positionCar)
+    {
+
+        float closestDistance = Mathf.Infinity;
+        Vector2 closestPoint = Vector2.zero;
+
+        foreach (Vector2 colliderPoint in collider.points)
+        {
+            Vector2 worldSpacePoint = collider.transform.TransformPoint(colliderPoint);
+            float distance = Vector2.Distance(worldSpacePoint, positionCar);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestPoint = worldSpacePoint;
+            }
+        }
+        Debug.Log("closestDistance="+closestDistance);
+
+        return closestPoint;
     }
 }
 
